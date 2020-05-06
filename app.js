8bd579814235445771a0d82ddc3af51fbd1fa84e
Marcos Lomboglia <marcos.lomboglia@gmail.com>
2020-05-06 18:23:15 +0100

    Yas server enabled

--- app.js
+++ app.js
@@ -10,22 +10,22 @@ yas.get('HQmmM_qwG4k', (err, data) => {
     console.log('GOT METADATA for HQmmM_qwG4k:', data || err)
   })
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.urlencoded());
+//const bodyParser = require('body-parser');
+//app.use(bodyParser.urlencoded({extended: true}));
+//app.use(express.urlencoded());
 
-const urlShortener = require('node-url-shortener');
+//const urlShortener = require('node-url-shortener');
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+//app.get('/', function(req, res) {
+//    res.sendFile(path.join(__dirname + '/index.html'));
+//});
 
-app.post('/url', function(req, res) {
-    const url = req.body.url;
+//app.post('/url', function(req, res) {
+//    const url = req.body.url;
+//
+//    urlShortener.short(url, function(err, shortUrl){
+//        res.send(shortUrl);
+//    });
+//});
 
-    urlShortener.short(url, function(err, shortUrl){
-        res.send(shortUrl);
-    });
-});
-
-app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
+yas.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
