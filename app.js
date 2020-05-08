48a34e1a933a65e421dc9ccfd266cb197e16374f
Marcos Lomboglia <marcos.lomboglia@gmail.com>
2020-05-08 08:57:14 +0100

    Log change

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ const port = process.env.PORT || 3000;
 const yas = require('youtube-audio-server')
 yas.setKey(process.env.YOUTUBE_API_KEY);
 
-yas.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
+yas.listen(port, () => console.log(`server listening on port ${port}!`));
