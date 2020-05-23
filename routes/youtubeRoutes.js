333b23b2c234b9762b0071091c00101f1aa675af
Marcos Lomboglia <marcos.lomboglia@gmail.com>
2020-05-23 15:46:28 +0100

    Health check endpoint

--- routes/youtubeRoutes.js
+++ routes/youtubeRoutes.js
@@ -6,6 +6,10 @@ router.get('/search/:query', youtubeController.searchVideos);
 
 router.get('/play/:videoId', youtubeController.playVideo);
 
+router.get('/', (req, res) => {
+    res.status(200).send({ message: 'OK'});
+});
+
 //router.get('/cache/:videoId', youtubeController.cacheVideo);
 
 module.exports = router;
